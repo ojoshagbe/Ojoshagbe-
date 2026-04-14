816500c4257b232f1691482913fcdbfae2485ee1
ojoshagbe <ojoshagbe1@gmail.com>
2026-04-14 16:35:04 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Knight Bot",
   botOwner: 'Professor', // Your name
-  ownerNumber: '2349029161634', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '2349069317625', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
